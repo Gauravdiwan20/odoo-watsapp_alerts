750a6d0fb8ce827d9eb5f86929eb86bbe5192fb5
Gauravdiwan20 <ytnone55@gmail.com>
2025-11-12 22:36:06 +0530

    Update __manifest__.py

--- addons/odoo_watsapp_alerts/__manifest__.py
+++ addons/odoo_watsapp_alerts/__manifest__.py
@@ -1,9 +1,11 @@
 {
-    'name': 'WhatsApp Alerts',
+    'name': 'Odoo WhatsApp Alerts',
     'version': '1.0',
-    'summary': 'Send WhatsApp notifications using Twilio',
+    'summary': 'Send WhatsApp alerts using Twilio API',
+    'description': 'This module sends WhatsApp messages using Twilio API.',
+    'author': 'Gaurav',
     'depends': ['base'],
     'data': [],
     'installable': True,
-    'application': False,
+    'application': True,
 }
