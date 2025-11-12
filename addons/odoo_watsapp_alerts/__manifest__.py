8385670ae300be8fa716dfb4be5e072ed46f29ba
Gauravdiwan20 <ytnone55@gmail.com>
2025-11-12 19:00:02 +0530

    Create __manifest__.py

---
+++ addons/odoo_watsapp_alerts/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'WhatsApp Alerts',
+    'version': '1.0',
+    'summary': 'Send WhatsApp notifications using Twilio',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
